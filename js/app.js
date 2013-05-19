a06d3584e9d7a7d83fa8910601e1a1a7bd0ec392
jB <jeremy.bates@gmail.com>
2013-05-19 09:41:57 -0400

    Revert "adding mongolab"
    This reverts commit 46a9e6c56cb7492f002a9875414a22f8dd673532.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /* NG */
-// Declare app level module which depends on filters, and services
+Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
 
 	config(['$routeProvider', function ($routeProvider) {
